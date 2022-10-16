96f587fbe67e45762e4553d2d79303aa1305e74d
Timofey Stolyar <112813059+TimofeySto@users.noreply.github.com>
2022-10-16 16:08:09 +0500

    создан скелет ReSTful сервиса для проекта планнер задач

---
+++ src/main/java/ru/javabegin/todo/backend/ToodBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.javabegin.todo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToodBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToodBackendApplication.class, args);
+    }
+
+}
